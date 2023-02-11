ae04cfd58ddde3f2815a81168487fc113226b641
tianyilim <0.tianyi.lim@gmail.com>
2023-02-11 20:31:38 +0100

    autolaunch everything in launch file

--- src/irobot_command/launch/task.launch.py
+++ src/irobot_command/launch/task.launch.py
@@ -1,8 +1,18 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+import os
 
 def generate_launch_description():
     return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('irobot_create_gazebo_bringup'), 'launch', 'create3_gazebo.launch.py')
+            ),
+        ),
         Node(
             package='irobot_command',
             namespace='',
