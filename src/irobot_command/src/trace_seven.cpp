519ae49711b82711ac9155b6670dffeec26f0dbc
tianyilim <0.tianyi.lim@gmail.com>
2023-02-11 17:41:10 +0100

    initial Commit

---
+++ src/irobot_command/src/trace_seven.cpp
@@ -0,0 +1,190 @@
+#include <cstdio>
+#include <functional>
+#include <memory>
+#include <future>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "irobot_create_msgs/action/navigate_to_position.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2/LinearMath/Matrix3x3.h"
+
+namespace irobot_command{
+
+class TraceSevenClient : public rclcpp::Node
+{
+public:
+    using NavToPos = irobot_create_msgs::action::NavigateToPosition;
+    using GoalHandleNavToPos = rclcpp_action::ClientGoalHandle<NavToPos>;
+
+    explicit TraceSevenClient(
+        const rclcpp::NodeOptions &node_options=rclcpp::NodeOptions()
+    ) : Node("trace_seven_client", node_options)
+    {
+        // Action client needs the action type `NavToPos`, the ROS node to add the client to (this), and the action name `/NavToPos`
+        this->client_ptr_ = rclcpp_action::create_client<NavToPos>(
+            this, "navigate_to_position"
+        );
+
+        // Initialize the timer which will let us call the next first action
+        this->timer_ = rclcpp::create_timer(
+            this, this->get_clock(),
+            std::chrono::milliseconds(500),
+            std::bind(&TraceSevenClient::send_goal, this) );
+
+        this->coord_idx_ = 0;   // Which coordinate in the "7" to move to
+        this->goal_done_ = false;
+    }
+
+    bool is_goal_done()
+    {
+        return this->goal_done_;
+    }
+
+    void send_goal()
+    {
+        using namespace std::placeholders;  // for defining callbacks later
+        this->timer_->cancel();             // Cancel timer so action only called once
+
+        // Wait for action client to start up
+        if (!this->client_ptr_)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Action client not initialized");
+        }
+
+        // Error! Action server might have crashed.
+        if (!this->client_ptr_->wait_for_action_server(std::chrono::seconds(10)))
+        {
+            RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
+            this->goal_done_ = true;
+            return;
+        }
+
+
+        auto goal_msg = NavToPos::Goal();
+        // goal_msg.goal_pose.header.stamp;
+        // goal_msg.goal_pose.header.frame_id = "";
+        goal_msg.achieve_goal_heading = true;
+        goal_msg.goal_pose.pose.position.x = this->x_coords[this->coord_idx_];
+        goal_msg.goal_pose.pose.position.y = this->y_coords[this->coord_idx_];
+        // Init quaternion
+        tf2::Quaternion q;
+        q.setRPY(0.0, 0.0, this->t_coords[this->coord_idx_]);
+        goal_msg.goal_pose.pose.orientation.w = q.getW();
+        goal_msg.goal_pose.pose.orientation.x = q.getX();
+        goal_msg.goal_pose.pose.orientation.y = q.getY();
+        goal_msg.goal_pose.pose.orientation.z = q.getZ();
+
+        RCLCPP_INFO(this->get_logger(), "Sending goal");
+
+        auto send_goal_options = rclcpp_action::Client<NavToPos>::SendGoalOptions();
+        send_goal_options.goal_response_callback =
+            std::bind(&TraceSevenClient::goal_response_callback, this, _1);   // the placeholders refer to the function arguments in the callback
+        send_goal_options.feedback_callback =
+            std::bind(&TraceSevenClient::feedback_callback, this, _1, _2);
+        send_goal_options.result_callback =
+            std::bind(&TraceSevenClient::result_callback, this, _1);
+
+        // Send goal to the action server
+        auto goal_handle_future = this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
+
+    }
+
+private:
+    rclcpp_action::Client<NavToPos>::SharedPtr client_ptr_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    bool goal_done_;
+    size_t coord_idx_;
+    std::vector<float> x_coords{-1.35, -1.35, -0.35, -1.15};
+    std::vector<float> y_coords{-1.0, -0.2, -0.7, 1.0};
+    std::vector<float> t_coords{90.0, -22.565, 115.201, -180.0};
+    // Hard-coded coordinates to get the robot to
+
+    void goal_response_callback(GoalHandleNavToPos::SharedPtr goal_handle)
+    {
+        if (!goal_handle) {
+            RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+        } else {
+            RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
+        }
+    }
+
+    void feedback_callback(
+        GoalHandleNavToPos::SharedPtr,
+        const std::shared_ptr<const NavToPos::Feedback> feedback)
+    {
+        std::string stat;
+        if (feedback->navigate_state == 2) {
+            stat = "%0.2f m", feedback->remaining_travel_distance;
+        } else {
+            stat = "%0.2f rad", feedback->remaining_angle_travel;
+        }
+
+        RCLCPP_INFO(
+            this->get_logger(),
+            "Nav state: %d, %s",
+            feedback->navigate_state,
+            stat
+        );
+    }
+
+    void result_callback(const GoalHandleNavToPos::WrappedResult & result)
+    {
+        switch (result.code) {
+        case rclcpp_action::ResultCode::SUCCEEDED:
+            this->coord_idx_++;
+            // We know we are done when we have reached the last element in the coordinates
+            if (this->coord_idx_ == this->x_coords.size()) {
+                this->goal_done_ = true;
+            } else {
+                this->send_goal();
+            }
+            break;
+        case rclcpp_action::ResultCode::ABORTED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+            return;
+        case rclcpp_action::ResultCode::CANCELED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+            return;
+        default:
+            RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+            return;
+        }
+
+        // Get euler angle from result callback
+        tf2::Quaternion q(
+            result.result->pose.pose.orientation.x,
+            result.result->pose.pose.orientation.y,
+            result.result->pose.pose.orientation.z,
+            result.result->pose.pose.orientation.w);
+        tf2::Matrix3x3 m(q);
+        double roll, pitch, yaw;
+        m.getRPY(roll, pitch, yaw);
+
+        RCLCPP_INFO(this->get_logger(), "Result received: x:%0.2f, y:%0.2f, th:%0.2f",
+            result.result->pose.pose.position.x,
+            result.result->pose.pose.position.y,
+            yaw);
+    }
+
+};  // class TraceSevenClient
+
+}
+
+int main(int argc, char ** argv)
+{
+    rclcpp::init(argc, argv);
+    auto action_client = std::make_shared<irobot_command::TraceSevenClient>();
+
+    while(!action_client->is_goal_done()){
+        rclcpp::spin_some(action_client);
+    }
+
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ src/irobot_command/src/undock_client.cpp
@@ -0,0 +1,132 @@
+#include <cstdio>
+#include <functional>
+#include <memory>
+#include <future>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "irobot_create_msgs/action/undock.hpp"
+
+namespace irobot_command{
+
+class iRobotUndockClient : public rclcpp::Node
+{
+public:
+    using Undock = irobot_create_msgs::action::Undock;
+    using GoalHandleUndock = rclcpp_action::ClientGoalHandle<Undock>;
+
+    explicit iRobotUndockClient(
+        const rclcpp::NodeOptions &node_options=rclcpp::NodeOptions()
+    ) : Node("irobot_undock_client", node_options)
+    {
+        // Action client needs the action type `Undock`, the ROS node to add the client to (this), and the action name `/undock`
+        this->client_ptr_ = rclcpp_action::create_client<Undock>(
+            this, "undock"
+        );
+
+        this->timer_ = rclcpp::create_timer(
+            this, this->get_clock(),
+            std::chrono::milliseconds(500),
+            std::bind(&iRobotUndockClient::send_goal, this) );
+    }
+
+    bool is_goal_done() const {return this->goal_done_;}
+
+    void send_goal()
+    {
+        using namespace std::placeholders;  // for defining callbacks later
+        this->timer_->cancel();             // Cancel timer so action only called once
+        this->goal_done_ = false;
+
+        // Wait for action client to start up
+        if (!this->client_ptr_)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Action client not initialized");
+        }
+
+        // Error! Action server might have crashed.
+        if (!this->client_ptr_->wait_for_action_server(std::chrono::seconds(10)))
+        {
+            RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
+            this->goal_done_ = true;
+            return;
+        }
+
+        auto goal_msg = Undock::Goal();     // This Action has no content.
+
+        RCLCPP_INFO(this->get_logger(), "Sending goal");
+
+        auto send_goal_options = rclcpp_action::Client<Undock>::SendGoalOptions();
+        send_goal_options.goal_response_callback =
+            std::bind(&iRobotUndockClient::goal_response_callback, this, _1);   // the placeholders refer to the function arguments in the callback
+        // send_goal_options.feedback_callback =
+        //     std::bind(&iRobotUndockClient::feedback_callback, this, _1, _2);
+        send_goal_options.result_callback =
+            std::bind(&iRobotUndockClient::result_callback, this, _1);
+
+        // Send goal to the action server
+        auto goal_handle_future = this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
+
+    }
+
+private:
+    rclcpp_action::Client<Undock>::SharedPtr client_ptr_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    bool goal_done_;
+
+    void goal_response_callback(GoalHandleUndock::SharedPtr goal_handle)
+    {
+        if (!goal_handle) {
+            RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
+        } else {
+            RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
+        }
+    }
+
+    // There is no feedback for this action.
+    // void feedback_callback(
+    //     GoalHandleUndock::SharedPtr,
+    //     const std::shared_ptr<const Undock::Feedback> feedback)
+    // {
+    //     // RCLCPP_INFO(
+    //     //     this->get_logger(),
+    //     //     "Next number in sequence received: %" PRId32,
+    //     //     feedback->sequence.back()
+    //     // );
+    // }
+
+    void result_callback(const GoalHandleUndock::WrappedResult & result)
+    {
+        this->goal_done_ = true;
+        switch (result.code) {
+        case rclcpp_action::ResultCode::SUCCEEDED:
+            break;
+        case rclcpp_action::ResultCode::ABORTED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+            return;
+        case rclcpp_action::ResultCode::CANCELED:
+            RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+            return;
+        default:
+            RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+            return;
+        }
+
+        RCLCPP_INFO(this->get_logger(), "Result received: is_docked=%s", result.result->is_docked ? "true" : "false");
+    }
+};  // class iRobotUndockClient
+
+}
+
+int main(int argc, char ** argv)
+{
+    rclcpp::init(argc, argv);
+    auto action_client = std::make_shared<irobot_command::iRobotUndockClient>();
+
+    while(!action_client->is_goal_done()){
+        rclcpp::spin_some(action_client);
+    }
+
+    rclcpp::shutdown();
+    return 0;
+}
